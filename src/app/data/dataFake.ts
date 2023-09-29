cef1c347eebe569d3c7cf3f8d227827e500d5af6
marcsonsantiago <marcsonsantiago@gmail.com>
2023-09-29 20:30:24 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"NOVO HOMEM DE FERRO EM 3D",
+    "description":"Marvel anuncia novo Homem de Ferro",
+    "photoCover":"https://media.contentapi.ea.com/content/dam/news/www-ea/common/ea-motive-new-title-teaser-16x9.jpg.adapt.1456w.jpg"
+  },
+  {
+    "id":"2",
+    "title":"Nova série da Marvel",
+    "description":"Marvel anuncia nova série do Loki",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"3",
+    "title":"Nova série na Disney+",
+    "description":"Marvel anuncia nova serie do Panera Negra na Disney+",
+    "photoCover":"https://img.elo7.com.br/product/original/37EBEF7/painel-1x65cm-pantera-negra-wakanda-festa-expressa.jpg"
+  },
+  {
+    "id":"4",
+    "title":"Nova série do X-MEN",
+    "description":"Nova série do X-MEN é anunciada pela Marvel",
+    "photoCover":"https://images.universohq.com/2023/09/Confins189_des01.jpg"
+  }
+]
